cdddaa52cf2b080645f7882515bf43efc1ff6885
Lucas Rosa <lucasrosa@Lucass-Mac-mini.local>
2014-04-17 18:54:04 -0300

    Added position script and a GameManager Position helper script.

---
+++ GameManagerPositionTouch.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+function Update () {
+	if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+		// Get the touch
+		var touch = Input.GetTouch(0);
+		// Transforms the touch in a Ray
+		var ray = Camera.main.ScreenPointToRay(touch.position);
+        var hit : RaycastHit;
+		// Verifies if the ray (touch) has touched any position
+    	if (Physics.Raycast (ray, hit) && hit.transform.gameObject.tag == "Position" ) {
+			// Deactivate all the positions
+			var positions = GameObject.FindGameObjectsWithTag("Position");
+			for (var position : GameObject in positions) {
+				position.GetComponent(Position).Deactivate();
+			}
+			
+			// raycast hit this gameobject so calling the WasTouched method of it
+			hit.transform.gameObject.GetComponent(Position).PositionWasTouched();
+         } else {
+         	// If touched anywhere else (not in a position), deactivate all the positions
+         	for (var position : GameObject in positions) {
+				position.GetComponent(Position).Deactivate();
+			}
+         }
+	}
+}
---
+++ Position.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+/* A position represents a "clickable" hexagon in the grid
+ */
+
+public var unit : GameObject; // The unit currently on this position (the owner)
+public var neighborPositions : GameObject[]; // The positions around this position, the ones that can be targeted by unit
+public var isOutpost : boolean = false; // Identify if this province is an outpost
+private var particlesPositionTouched : GameObject; // Active after this position was touched
+private var particlesNeighborTouched : GameObject; // Active after a neighbor of this position was touched
+
+
+function Start () {
+	// Sets the particles
+	for (var child : Transform in transform) {
+		if (child.name == "ParticlesPositionTouched") {
+			particlesPositionTouched = child.gameObject;
+		} else if (child.name == "ParticlesNeighborTouched") {
+			particlesNeighborTouched = child.gameObject;
+		}
+	}
+}
+
+function PositionWasTouched () {
+	// Set active the particlesPositionTouched
+	particlesPositionTouched.SetActive(true);
+	
+	// Call the methods on the neighbors
+	for (var neighbor : GameObject in neighborPositions) {
+		neighbor.GetComponent(Position).NeighborWasTouched();
+	}
+}
+
+function NeighborWasTouched () {
+	// Set active the particlesPositionTouched
+	particlesNeighborTouched.SetActive(true);
+}
+
+function Deactivate () {
+	// Deactive particlesPositionTouched and particlesPositionTouched
+	particlesPositionTouched.SetActive(false);
+	particlesNeighborTouched.SetActive(false);
+}
